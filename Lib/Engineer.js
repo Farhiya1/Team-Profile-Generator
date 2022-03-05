aa69a9b8a662a3dd2432b5b1e28d8a5d96d3c0f4
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-03-05 16:12:29 +0000

    Engineer class extending from employee

---
+++ Lib/Engineer.js
@@ -0,0 +1,20 @@
+// Gets the class employee from the file path.
+const Employee = require("./Employee");
+
+// Extends Engineer from Employee and allows extraction of information using constructor function
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+
+  getGithub() {
+    return this.github;
+  }
+
+  getRole() {
+    return "Engineer";
+  }
+}
+
+module.exports = Engineer;
